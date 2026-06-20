d0432f313eb088fda4f24098e0e7491b27cbab99
cristian-95 <cristian_santos95@outlook.com>
2026-06-19 22:52:06 -0300

    chore: configuração do 'context-path' e containers

--- src/main/java/com/cristian/shortin_api/ShortinApiApplication.java
+++ src/main/java/com/cristian/shortin_api/ShortinApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShortinApiApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ShortinApiApplication.class, args);
 	}
